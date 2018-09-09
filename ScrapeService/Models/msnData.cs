6aa2c5d14e2244bf2918f0934bf1b341b24509e0
Pratik Khandelwal <prkhandelwal@hotmail.com>
2018-09-09 23:24:59 +0530

    Added Model in Scrape Service

---
+++ ScrapeService/Models/msnData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScrapeService.Models
+{
+    public class msnData
+    {
+        public string Category { get; set; }
+        public string InnerText { get; set; }
+        public string href { get; set; }
+    }
+}
