13b921c0de881e4af319fd3fbc1f8c3a8e0d3588
Pratik Khandelwal <prkhandelwal@hotmail.com>
2018-09-09 23:42:04 +0530

    Twiiter Search Stuff

---
+++ ScrapeService/Twitter/Search.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Azure.CognitiveServices.Language.TextAnalytics.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweetinvi;
+
+namespace ScrapeService.Twitter
+{
+    public class Search
+    {
+        public static IEnumerable<Tweetinvi.Models.ITweet> Tweets;
+
+        public async static Task<List<MultiLanguageInput>> GetTweets(string query)
+        {
+            List<MultiLanguageInput> TweetInput = new List<MultiLanguageInput>();
+            Auth.SetUserCredentials(creds.ConsumerKey, creds.ConsumerSecret, creds.AccessToken, creds.AccessSecret);
+            //var tweets = await SearchAsync.SearchTweets(query);
+            Tweets = await SearchAsync.SearchTweets(query);
+            List<Tweetinvi.Models.ITweet> tweetList = Tweets.ToList();
+            int i = 0;
+            foreach (Tweetinvi.Models.ITweet item in tweetList)
+            {
+                var k = new MultiLanguageInput("en", i.ToString(), item.FullText);
+                TweetInput.Add(k);
+                i++;
+            }
+            return TweetInput;
+        }
+    }
+}
---
+++ ScrapeService/Twitter/creds.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScrapeService.Twitter
+{
+    public class creds
+    {
+        public const String ConsumerKey = "FCu0D5jI5wiUo5xIFTe8eFnDm";
+
+        public const String ConsumerSecret = "tvj1LRxJl9WnO2KxM03iCSReXs0e6Taxv4sz3EBpm1SBARXbHn";
+
+        public const String Owner = "K2Prk";
+
+        public const String OwnerId = "4672579898";
+
+        public const String AccessToken = "4672579898-iv7HXCPphzlNzEnEPQnDpX0dUwZN5PBYDSmWWkX";
+
+        public const String AccessSecret = "3tJrZt6lxYckOHjKps0Uslm8vChHscT2AkKB4tbUy8gR8";
+    }
+}
