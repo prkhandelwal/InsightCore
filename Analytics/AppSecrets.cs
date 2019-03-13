d2dc1cce88b2ee8d38f042b42ae65faff8fe5636
Pratik <prkhandelwal@hotmail.com>
2019-03-13 21:42:40 +0530

    Added api key class

---
+++ Analytics/AppSecrets.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Analytics
+{
+    public class AppSecrets
+    {
+        public static string CognitiveApiKeys = "Insert your API keys here";
+    }
+}
